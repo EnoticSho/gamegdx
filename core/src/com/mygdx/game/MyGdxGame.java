b650f3c5e0e3fa0218a296463afacf700e09533f
sergey turpakov <paradise666hell@mail.ru>
2022-05-28 23:53:56 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	float time;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+
+
+		float x = Gdx.input.getX();
+		float y = Gdx.graphics.getHeight()-Gdx.input.getY();
+
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) Gdx.app.exit();
+
+		time += Gdx.graphics.getDeltaTime();
+		if (time >= 5) Gdx.app.exit();
+
+		batch.begin();
+		batch.draw(img, x, y);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
